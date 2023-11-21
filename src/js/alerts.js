f4253e8c69beb21f0ee0f2ec20754e6dfa5618d3
Aljodor21 <alejandro_toro23202@elpoli.edu.co>
2023-11-20 19:07:48 -0500

    Esto es una actualización del frontend para la página de nuestro profesor y la asociación de proyectos

---
+++ src/js/alerts.js
@@ -0,0 +1,18 @@
+const divAlert = document.getElementById('alert-border');
+
+const clickB = document.getElementById('clicker');
+
+
+clickB.addEventListener('click',()=>{
+    divAlert.classList.add('invisible');
+});
+
+
+const divAlert2 = document.getElementById('alert-border2');
+
+const clickB2 = document.getElementById('clicker2');
+
+
+clickB2.addEventListener('click',()=>{
+    divAlert2.classList.add('invisible');
+});
